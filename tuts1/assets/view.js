a155575880357d9d1cb3feb6eabebf746365352c
kwabena-aboah <obed30mintah@yahoo.com>
2019-07-06 21:57:06 +0000

    initial commit

---
+++ tuts1/assets/view.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let $ = require('jquery')
+let count = 0
+$('#click-counter').text(count.toString())
+$('#countbtn').on('click', () =>{
+  count ++
+  $('#click-counter').text(count)
+})
